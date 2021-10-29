efd4079c6fff023474308fda80fb613cf1c7f7c4
minhoen15 <vuminhhoan1510@gmail.com>
2021-10-29 09:37:08 +0700

    sua trang chu

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['login_check'])) {
-  header("location:login.php");
+    header("location:login.php");
 }
 ?>
 <?php
@@ -10,39 +10,39 @@ include('./bridge/menu.php')
 <div class="row" style="
         margin-right: -15px;
         margin-left: -15px;">
-        <div class="col-md-3 col-sm-4 col-xs-12 margin-top-10">
-            <div class="dash-let">
-                <a href="studentmarks.php">
-                    <i class="fa fa-university"></i>
-                    <span>Tra cứu điểm</span>
-                </a>
-            </div>
-        </div>
-        <div class="col-md-3 col-sm-4 col-xs-12 margin-top-10">
-            <div class="dash-let">
-                <a href="accounts.php">
-                    <i class="fa fa-university"></i>
-                    <span>Quản lí tài khoản</span>
-                </a>
-            </div>
-        </div>
-        <div class="col-md-3 col-sm-4 col-xs-12 margin-top-10">
-            <div class="dash-let">
-                <a href="#">
-                    <i class="fa fa-university"></i>
-                    <span>Thông tin học sinh</span>
-                </a>
-            </div>
-        </div>
-        <div class="col-md-3 col-sm-4 col-xs-12 margin-top-10">
-            <div class="dash-let">
-                <a href="contact.php">
-                    <i class="fa fa-university"></i>
-                    <span>Liên hệ</span>
-                </a>
-            </div>
+    <div class="col-md-3 col-sm-4 col-xs-12 margin-top-10">
+        <div class="dash-let">
+            <a href="studentmarks.php">
+                <i class="fa fa-university"></i>
+                <span>Kết quả đăng kí học </span>
+            </a>
         </div>
     </div>
+    <div class="col-md-3 col-sm-4 col-xs-12 margin-top-10">
+        <div class="dash-let">
+            <a href="accounts.php">
+                <i class="fa fa-university"></i>
+                <span>Quản lí tài khoản</span>
+            </a>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-4 col-xs-12 margin-top-10">
+        <div class="dash-let">
+            <a href="#">
+                <i class="fa fa-university"></i>
+                <span>Thông tin học sinh</span>
+            </a>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-4 col-xs-12 margin-top-10">
+        <div class="dash-let">
+            <a href="contact.php">
+                <i class="fa fa-university"></i>
+                <span>Liên hệ</span>
+            </a>
+        </div>
+    </div>
+</div>
 </div>
 <?php
 include('./bridge/footer.php')
