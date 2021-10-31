bc4d42b2e5d78e76b9b7884e0ada551b2ae12987
minhoen15 <vuminhhoan1510@gmail.com>
2021-10-31 12:31:19 +0700

    them trang chu giang vien

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-if (!isset($_SESSION['login_check'])) {
-    header("location:login.php");
-}
+//if (!isset($_SESSION['login_check'])) {
+// header("location:login.php");
+//}
 ?>
 <?php
 include('./bridge/menu.php')
