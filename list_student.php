7388880189930ee7b5a69e5d023d64899e69503c
minhoen15 <vuminhhoan1510@gmail.com>
2021-11-04 09:55:16 +0700

    danh sach sinh vien dang ki

--- list_student.php
+++ list_student.php
@@ -27,10 +27,9 @@ include('./bridge/menu.php');
 
                         //bước 2 khai báo câu lệnh thực thi và thực hiện truy vấn
                         $sql = "
-                        SELECT id_assign,code_course,name_course,days,lesson,startdate,enddate,name_room,name_semester,credit 
-                        FROM tb_users,tb_course,tbl_assign,tbl_room,tbl_semester 
-                        where tbl_assign.id_course=tb_course.id_course and tbl_assign.id_user = tb_users.id_user and tb_course.id_room = tbl_room.id_room and tb_course.id_semester = tbl_semester.id_semester and tbl_assign.id_user=$id_user";
-
+                        SELECT fullname, name_course, email, sex, birthdate, phone FROM tb_users, tb_course, tbl_assign, tbl_register 
+                        WHERE tb_course.id_course=tbl_assign.id_course AND tb_users.id_user=tbl_register.id_user
+                         AND tb_users.id_user=tbl_assign.id_user AND tbl_register.status = 1";
                         $result = mysqli_query($conn, $sql);
 
                         //bước 3 xử lý kết quả trả về
@@ -43,15 +42,13 @@ include('./bridge/menu.php');
                                 <tr>
                                     <th scope="row"><?php echo $i; ?> </th>
 
-                                    <td><?php echo $row['code_course']; ?> </td>
+                                    <td><?php echo $row['fullname']; ?> </td>
                                     <td><?php echo $row['name_course']; ?> </td>
-                                    <td><?php echo $row['days']; ?> </td>
-                                    <td><?php echo $row['lesson']; ?> </td>
-                                    <td><?php echo $row['name_room']; ?> </td>
-                                    <td><?php echo $row['name_semester']; ?> </td>
-                                    <td><?php echo $row['startdate']; ?> </td>
-                                    <td><?php echo $row['enddate']; ?> </td>
-                                    <td><?php echo $row['credit']; ?> </td>
+                                    <td><?php echo $row['email']; ?> </td>
+                                    <td><?php echo $row['sex']; ?> </td>
+                                    <td><?php echo $row['birthdate']; ?> </td>
+                                    <td><?php echo $row['phone']; ?> </td>
+
                                 </tr>
                         <?php
                                 $i++;
